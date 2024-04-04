9c76c88ece97477584e16abedb25547b8246acf4
Diego García Currás <d.garciac.2021@alumnos.urjc.es>
2024-04-04 20:29:28 +0200

    Update launcher!

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Humbly modified in 2024 by Diego García Currás.
 
 import os
 from os import environ, pathsep
